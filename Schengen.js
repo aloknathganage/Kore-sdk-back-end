2040e5f5cc19950cb4008b0e52b2cf5ab8c8ff85
Aloknath Ganage <69706421+aloknathganage@users.noreply.github.com>
2024-10-31 11:08:26 +0530

    Add files via upload

---
+++ Schengen.js
@@ -0,0 +1,225 @@
+const schengen = [
+  {
+    "CommonTypesID": 1535,
+    "code": 23,
+    "description": "Austria",
+    "MasterType": "Schengen Plan",
+    "isdeleted": 0,
+    "effectivedate": "2013-06-12 11:02:00"
+  },
+  {
+    "CommonTypesID": 1536,
+    "code": 129,
+    "description": "Estonia",
+    "MasterType": "Schengen Plan",
+    "isdeleted": 0,
+    "effectivedate": "2013-06-12 11:02:00"
+  },
+  {
+    "CommonTypesID": 1537,
+    "code": 12,
+    "description": "Greece",
+    "MasterType": "Schengen Plan",
+    "isdeleted": 0,
+    "effectivedate": "2013-06-12 11:02:00"
+  },
+  {
+    "CommonTypesID": 1538,
+    "code": 128,
+    "description": "Latvia",
+    "MasterType": "Schengen Plan",
+    "isdeleted": 0,
+    "effectivedate": "2013-06-12 11:02:00"
+  },
+  {
+    "CommonTypesID": 1539,
+    "code": 13,
+    "description": "Netherlands",
+    "MasterType": "Schengen Plan",
+    "isdeleted": 0,
+    "effectivedate": "2013-06-12 11:02:00"
+  },
+  {
+    "CommonTypesID": 1540,
+    "code": 241,
+    "description": "Slovakia",
+    "MasterType": "Schengen Plan",
+    "isdeleted": 0,
+    "effectivedate": "2013-06-12 11:02:00"
+  },
+  {
+    "CommonTypesID": 1541,
+    "code": 14,
+    "description": "Belgium",
+    "MasterType": "Schengen Plan",
+    "isdeleted": 0,
+    "effectivedate": "2013-06-12 11:02:00"
+  },
+  {
+    "CommonTypesID": 1542,
+    "code": 125,
+    "description": "Finland",
+    "MasterType": "Schengen Plan",
+    "isdeleted": 0,
+    "effectivedate": "2013-06-12 11:02:00"
+  },
+  {
+    "CommonTypesID": 1543,
+    "code": 18,
+    "description": "Hungary",
+    "MasterType": "Schengen Plan",
+    "isdeleted": 0,
+    "effectivedate": "2013-06-12 11:02:00"
+  },
+  {
+    "CommonTypesID": 1544,
+    "code": 127,
+    "description": "Lithuania",
+    "MasterType": "Schengen Plan",
+    "isdeleted": 0,
+    "effectivedate": "2013-06-12 11:02:00"
+  },
+  {
+    "CommonTypesID": 1545,
+    "code": 27,
+    "description": "Norway",
+    "MasterType": "Schengen Plan",
+    "isdeleted": 0,
+    "effectivedate": "2013-06-12 11:02:00"
+  },
+  {
+    "CommonTypesID": 1546,
+    "code": 139,
+    "description": "Slovenia",
+    "MasterType": "Schengen Plan",
+    "isdeleted": 0,
+    "effectivedate": "2013-06-12 11:02:00"
+  },
+  {
+    "CommonTypesID": 1547,
+    "code": 142,
+    "description": "Czech Republic",
+    "MasterType": "Schengen Plan",
+    "isdeleted": 0,
+    "effectivedate": "2013-06-12 11:02:00"
+  },
+  {
+    "CommonTypesID": 1548,
+    "code": 15,
+    "description": "France",
+    "MasterType": "Schengen Plan",
+    "isdeleted": 0,
+    "effectivedate": "2013-06-12 11:02:00"
+  },
+  {
+    "CommonTypesID": 1549,
+    "code": 121,
+    "description": "Iceland",
+    "MasterType": "Schengen Plan",
+    "isdeleted": 0,
+    "effectivedate": "2013-06-12 11:02:00"
+  },
+  {
+    "CommonTypesID": 1550,
+    "code": 119,
+    "description": "Luxembourg",
+    "MasterType": "Schengen Plan",
+    "isdeleted": 0,
+    "effectivedate": "2013-06-12 11:02:00"
+  },
+  {
+    "CommonTypesID": 1551,
+    "code": 28,
+    "description": "Poland",
+    "MasterType": "Schengen Plan",
+    "isdeleted": 0,
+    "effectivedate": "2013-06-12 11:02:00"
+  },
+  {
+    "CommonTypesID": 1552,
+    "code": 17,
+    "description": "Spain",
+    "MasterType": "Schengen Plan",
+    "isdeleted": 0,
+    "effectivedate": "2013-06-12 11:02:00"
+  },
+  {
+    "CommonTypesID": 1553,
+    "code": 25,
+    "description": "Denmark",
+    "MasterType": "Schengen Plan",
+    "isdeleted": 0,
+    "effectivedate": "2013-06-12 11:02:00"
+  },
+  {
+    "CommonTypesID": 1554,
+    "code": 29,
+    "description": "Germany",
+    "MasterType": "Schengen Plan",
+    "isdeleted": 0,
+    "effectivedate": "2013-06-12 11:02:00"
+  },
+  {
+    "CommonTypesID": 1555,
+    "code": 19,
+    "description": "Italy",
+    "MasterType": "Schengen Plan",
+    "isdeleted": 0,
+    "effectivedate": "2013-06-12 11:02:00"
+  },
+  {
+    "CommonTypesID": 1556,
+    "code": 123,
+    "description": "Malta",
+    "MasterType": "Schengen Plan",
+    "isdeleted": 0,
+    "effectivedate": "2013-06-12 11:02:00"
+  },
+  {
+    "CommonTypesID": 1557,
+    "code": 118,
+    "description": "Portugal",
+    "MasterType": "Schengen Plan",
+    "isdeleted": 0,
+    "effectivedate": "2013-06-12 11:02:00"
+  },
+  {
+    "CommonTypesID": 1558,
+    "code": 26,
+    "description": "Sweden",
+    "MasterType": "Schengen Plan",
+    "isdeleted": 0,
+    "effectivedate": "2013-06-12 11:02:00"
+  },
+  {
+    "CommonTypesID": 1559,
+    "code": 22,
+    "description": "Switzerland",
+    "MasterType": "Schengen Plan",
+    "isdeleted": 0,
+    "effectivedate": "2013-06-12 11:02:00"
+  },
+  {
+    "CommonTypesID": 3710,
+    "code": 143,
+    "description": "Liechtenstein",
+    "MasterType": "Schengen Plan",
+    "isdeleted": 0,
+    "effectivedate": "2019-06-14 11:02:00"
+  },
+  {
+    "CommonTypesID": 16836,
+    "code": 138,
+    "description": "Croatia",
+    "MasterType": "Schengen Plan",
+    "isdeleted": 0,
+    "effectivedate": "2023-08-23 12:51:00"
+  }
+]
+
+const schengenCountries = schengen.map(item => ({
+  code: item.code,
+  description: item.description
+}));
+
+module.exports = schengenCountries;
